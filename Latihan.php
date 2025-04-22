6b93d5563ac446e46b0e2ff60d37b699b96d746c
Variel Axcelino <varielaxcelinowhitejemus_2428250032@mhs.mdp.ac.id>
2025-04-22 16:39:39 +0700

    Add files via upload

---
+++ Latihan.php
@@ -0,0 +1,31 @@
+<?php
+echo "Variel Axcelino White Jemus";
+echo "<br/>";
+echo "2428250032";
+echo "<br/>";
+// Soal No 1
+$tahunlahir = 2006;
+$tahunsekarang = 2025;
+$umur = $tahunsekarang - $tahunlahir;
+echo "1. Umur saya $umur";
+
+echo "<br/>";
+// Soal No 2
+$usd = 1;
+$idr = 16.863 * $usd;
+echo "2. Konversi Mata Uang Usd =$idr";
+
+echo "<br/>";
+//Soal No 3
+$celcius = 0;
+$F =($celcius * 9/5) + 32;
+echo "3. Konversi dari celcius ke farenheit adalah $F";
+
+echo "<br/>";
+//Soal no 4
+$pi = 3.14;
+$r = 4;
+$L = $pi * $r * $r;
+echo "4. Luas Lingkaran = $L";
+
+?>
