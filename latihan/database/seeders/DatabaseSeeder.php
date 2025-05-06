85dd7a93007bb5c7dc6a904367a16c32edf16f2c
Variel225 <varielaxcelinowhitejemus_2428250032@mhs.mdp.ac.id>
2025-05-06 15:12:21 +0700

    belajar migration and seed

--- latihan/database/seeders/DatabaseSeeder.php
+++ latihan/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\mahasiswa;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use DB;
+use Hash;
 use Illuminate\Database\Seeder;
+use Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +19,44 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        //User::factory()->create([
+        //   'name' => 'Test User',
+        //    'email' => 'test@example.com',
+        //]);
+
+        DB::table("users")->insert([
+            'nama' => Str::random(10),
+            'email' => Str::random(10). "@example.com",
+            'password' => Hash::make('password'),
         ]);
+
+        //insert data mahasiswa menggunakan query builder
+        //DB::table('mahasiswa')->insert([
+        //   'npm' => "2428250032",
+        //    'nama_mahasiswa' => "Variel Axcelino White Jemus",
+        //    'born_date' => "Korea",
+        //    'tanggal_lahir' => "2006-01-01",
+        //    'alamat' => "Palembang",
+        //    'created_at' => date("y-m-d H:i:s")
+        //]);
+
+        //DB::table('mahasiswa')
+        //->where("npm", "2428250032")
+        //->update(["npm" => "2428250032"]);
+
+        mahasiswa::insert([
+             'npm' => "2428250032",
+            'nama_mahasiswa' => "Variel Axcelino White Jemus",
+            'born_date' => "Korea",
+            'tanggal_lahir' => "2006-01-01",
+            'alamat' => "Palembang",
+            'created_at' => date("y-m-d H:i:s")
+        ]);
+
+        //retrive all data
+        mahasiswa::all();
+        mahasiswa::where('id', '<', 3)->get(); //select * from mahasiswa where id < 3
+
+        mahasiswa::select(['npm', 'nama'])->get(); //select npm,nama for mahasiswa
     }
 }
